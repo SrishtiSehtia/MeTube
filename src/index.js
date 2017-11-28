65118b5024b6b8cf6ea450eac2de0187f461264c
Srishti Sehtia <srishti0123@gmail.com>
2017-11-28 11:26:04 -0800

    include YouTube search API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyDqZRnd1ckcqXMS9_pVqSGtvf3ldRKPL00'
+
 // Create a new component. This component should produce some HTML
 
 const App = () => {
