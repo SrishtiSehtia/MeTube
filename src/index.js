09767f1e5fa5a40bf7eca03c8aa5d623392221ae
Srishti Sehtia <srishti0123@gmail.com>
2017-12-14 12:28:13 -0800

    pass selected video into VideoDetail

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList videos={this.state.videos} />
       </div>
     );
